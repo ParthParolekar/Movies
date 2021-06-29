6bc1dd1a5993610dc6f77b9e57a135457e0e8c01
ParthParolekar <parolekarparth@gmail.com>
2021-06-29 15:11:47 +0530

    add More Button feature

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const searchInput = document.querySelector("#search-input");
 const form = document.querySelector(".search-form");
 let fetchLink;
 const imgURL = "https://image.tmdb.org/t/p/w500";
+const moreBtn = document.querySelector(".more-btn");
+let page = 1;
+
+//event listeners
+moreBtn.addEventListener("click", showMore);
+
+function showMore() {
+  page++;
+  fetchLink = `https://api.themoviedb.org/3/movie/popular?api_key=59441c8d9904e75ec235032cfdb2ea10&language=en-US&page=${page}`;
+  getMovies(fetchLink);
+}
+
+function popularMovies() {
+  fetchLink = `https://api.themoviedb.org/3/movie/popular?api_key=59441c8d9904e75ec235032cfdb2ea10&language=en-US&page=1`;
+
+  getMovies(fetchLink);
+}
+
 function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
@@ -11,12 +29,6 @@ function getMovies(url) {
       console.log(data.results);
       showMovies(data.results);
     });
-}
-
-function popularMovies() {
-  fetchLink = `https://api.themoviedb.org/3/movie/popular?api_key=59441c8d9904e75ec235032cfdb2ea10&language=en-US&page=1`;
-
-  getMovies(fetchLink);
 }
 
 function showMovies(data) {
@@ -40,45 +52,3 @@ function showMovies(data) {
 }
 
 popularMovies();
-// // async function fetchApi(url) {
-// //   const dataFetch = await fetch(url, {
-// //     method: "GET",
-// //     headers: { Accept: "application/json", Authorization: auth },
-// //   });
-// //   data = await dataFetch.json();
-// //   return data;
-// // }
-
-// function displayMovies(data) {
-//   data.results.forEach((movie) => {
-//     const movieCard = document.createElement("div");
-//     movieCard.classList.add(".movie-card");
-//     movieCard.innerHTML = `
-//         <div class="poster">
-//           <img
-//             class="movie-poster"
-//             src="C:\Users\prasad\Desktop\poster.jpg"
-//             alt="Movie Poster"
-//           />
-//         </div>
-//         <div class="movie-info">
-//           <div class="movie-name-year">
-//             <h2 class="movie-name">${movie.original_title}</h2>
-//             <span class="movie-year">${movie.release_date}</span>
-//           </div>
-//           <div class="rating">
-//             <span class="movie-rating">${movie.vote_average}</span>
-//           </div>
-//         </div>`;
-//   });
-// }
-
-// async function popularMovies() {
-//   fetchLink = `https://api.themoviedb.org/3/movie/popular?api_key=59441c8d9904e75ec235032cfdb2ea10&language=en-US&page=1`;
-
-//   const film = await fetchApi(fetchLink);
-//   console.log(film);
-//   displayMovies(film);
-// }
-
-// popularMovies();
